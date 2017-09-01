90ba27e1264fd17f6e2e9afd243f09fb87fe3629
Maryussuperboy <martsyalis@gmail.com>
2017-09-01 14:35:44 -0700

    final clean

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -34,7 +34,9 @@ Article.prototype.toHtml = function() {
 // and use it to instantiate all the articles. This code is moved from elsewhere, and
 // encapsulated in a simply-named function for clarity.
 Article.loadAll = function(rawData) {
+  console.log("rawData:",rawData);
   rawData.sort(function(a,b) {
+    
     return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
   });
 
@@ -47,23 +49,22 @@ Article.loadAll = function(rawData) {
 // and process it, then hand off control to the View.
 Article.fetchAll = function() {
   if (localStorage.rawData) {
+    console.log("weve done it!");
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
     Article.loadAll(JSON.parse(localStorage.rawData)); //TODO-DONE: What do we pass in to loadAll()?
-    //TODO: What method do we call to render the index page?
-    articleView.inItIndexPage();
-  }
-  else {
+    //TODO-DONE: What method do we call to render the index page?
+    articleView.initIndexPage();
+  } else {
     $.getJSON( '/data/hackerIpsum.json' )
       .done( function ( data ){
-        console.log( 'data',data );
         localStorage.setItem ( 'data', JSON.stringify( data ) );
-        Article.all.push( Article.toHtml() );
-     
-        articleView.inItIndexPage();
-      })
-    // TODO: When we don't already have the rawData,
+        Article.loadAll(data);
+        articleView.initIndexPage();
+      });
+    
+    // TODO-DONE: When we don't already have the rawData,
     // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
