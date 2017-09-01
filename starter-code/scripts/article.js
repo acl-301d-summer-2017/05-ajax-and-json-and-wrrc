19a3163c9ebf6f56c1b9aa111aded5ee151815a6
Robin Miller <rmillerpdx@gmail.com>
2017-09-01 13:51:23 -0700

    add else{} code to fetchAll method

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -54,7 +54,12 @@ Article.fetchAll = function() {
     
     //TODO: What method do we call to render the index page?
   } else {
-    localStorage.setItem('rawData', JSON.stringify(rawData))
+    $.getJSON( 'rawData' )
+      .done( function(data){
+        console.log('success');
+        localStorage.setItem('rawData', JSON.stringify( data ));
+        Article.loadAll( data );
+      })
     // DONE TODO: When we don't already have the rawData,
     // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
     // cache it in localStorage so we can skip the server call next time,
